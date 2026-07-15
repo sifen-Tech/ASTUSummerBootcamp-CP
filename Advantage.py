cbfcb5b36e057b208b8a239856f3bcb1a42a6a2d
sifen-Tech <sifenbeyan@gmail.com>
2026-07-15 20:58:54 +0300

    Create  Advantage.py

---
+++ Advantage.py
@@ -0,0 +1,12 @@
+for _ in range(int(input())):
+    n=int(input())
+    arr=list(map(int,input().split()))
+    arr2=arr[:]
+    arr2.sort()
+    ans=[]
+    for i in arr:
+        if i == arr2[-1]:
+           ans.append(i-arr2[-2])
+        else:
+              ans.append(i-arr2[-1])
+    print(*ans)
