2f3216dd22522837b25adbd2d4130171a3563188
sifen-Tech <sifenbeyan@gmail.com>
2026-08-05 21:23:46 +0300

    Time: 127 ms (65.56%), Space: 19.8 MB (76.13%) - LeetHub

--- 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.py
+++ 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.py
@@ -8,10 +8,7 @@ class Solution:
         for right in range(n):
             counter[s[right]] -= 1
             while left < n and 4*max(counter.values()) <= n:
-
-           
                 ans = min(ans, right-left+1) 
                 counter[s[left]] += 1 
                 left += 1
-
         return ans
